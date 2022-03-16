9861d1cd3f51c54dc2c92e9c75e923ad4724d6fa
Usama-Yasir <usamayasir32@gmail.com>
2022-03-17 04:32:02 +0500

    updated commit

--- script.js
+++ script.js
@@ -2,13 +2,47 @@ const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.d
 
 const IMGPATH = "https://image.tmdb.org/t/p/w1280/";
 
+const main = document.querySelector("main");
+
 async function getMovies() {
     const resp = await fetch (APIURL);
     const respData = await resp.json();
 
     console.log(respData);
 
-    return respData;
+    respData.results.forEach((movie) => {
+        const { poster_path, title, vote_average } = movie;
+        
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+
+        movieEl.innerHTML = `
+        <img
+        src="${IMGPATH + poster_path}" 
+        alt="${title}"
+        />
+       <div class="movie-info">
+           <h3>${title}Movie title</h3>
+           <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+       </div>
+        `;
+
+        main.appendChild(movieEl);
+    });
+
+   return respData;
+}
+
+function getClassByRate(vote) {
+    if(vote > 8) {
+        return "green";
+    }
+    else if (vote >= 5){
+        return "orange";
+    }
+    else{
+        return "red";
+    }
 }
 
 getMovies();
